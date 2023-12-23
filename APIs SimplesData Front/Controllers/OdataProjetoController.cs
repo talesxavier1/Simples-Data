082412c0b938d7e7f4dc9350b045399c0e9473b9
Tales Xavier <talesxavier1@hotmail.com>
2023-12-22 23:15:50 -0300

    Remoção do acesso statico para a utilização da injeção de dependencia.

--- APIs SimplesData Front/Controllers/OdataProjetoController.cs
+++ APIs SimplesData Front/Controllers/OdataProjetoController.cs
@@ -4,18 +4,15 @@ using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
 using MongoDB.Driver;
 using Simples_Data.Models;
-using SingularChatAPIs.BD;
 
 namespace Simples_Data.APIs_SimplesData_Front.Controllers;
 [ApiController]
 [Route("odata/v1")]
 public class OdataProjetoController : ODataController {
     private IMongoCollection<ProjetoModel> projetoCollection;
-    private IMongoDatabase mongoDataBase;
 
-    public OdataProjetoController() {
-        this.mongoDataBase = MongoDBConnection.getMongoDatabase();
-        this.projetoCollection = mongoDataBase.GetCollection<ProjetoModel>("Projetos");
+    public OdataProjetoController(IMongoDatabase mongoDatabase) {
+        this.projetoCollection = mongoDatabase.GetCollection<ProjetoModel>("Projetos");
     }
 
     [EnableQuery]
