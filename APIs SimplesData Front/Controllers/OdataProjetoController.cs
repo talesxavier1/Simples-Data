5a99998921c5456f292d6198198d0666e90834e2
Tales Xavier <talesxavier1@hotmail.com>
2023-12-24 22:08:48 -0300

    Melhoria. Aplicação do padrão Repository.

--- APIs SimplesData Front/Controllers/OdataProjetoController.cs
+++ APIs SimplesData Front/Controllers/OdataProjetoController.cs
@@ -4,24 +4,23 @@ using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
 using MongoDB.Driver;
 using Simples_Data.Models;
+using Simples_Data.Repository.Implementations;
 
 namespace Simples_Data.APIs_SimplesData_Front.Controllers;
 [ApiController]
 [Route("odata/v1")]
 public class OdataProjetoController : ODataController {
-    private IMongoCollection<ProjetoModel> projetoCollection;
+    private IMongoCollection<ProjetoModel> _projetoCollection;
 
     public OdataProjetoController(IMongoDatabase mongoDatabase) {
-        this.projetoCollection = mongoDatabase.GetCollection<ProjetoModel>("Projetos");
+        this._projetoCollection = mongoDatabase.GetCollection<ProjetoModel>("Projetos");
     }
 
     [EnableQuery]
     [HttpGet("ProjetoModels")]
     async public Task<IEnumerable<ProjetoModel>> Get() {
-
-        var filter = Builders<ProjetoModel>.Filter.Eq(DOC => DOC.dataInfoModel.active,true);
-        var cursor = await projetoCollection.FindAsync(filter);
-        return await cursor.ToListAsync();
+        var result = await new ProjetoRepository(_projetoCollection).GetAll();
+        return result;
     }
 
 }
---
+++ Repository/Implementations/ProjetoRepository.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using Simples_Data.Models;
+
+namespace Simples_Data.Repository.Implementations;
+public class ProjetoRepository : IProjetoRepository {
+
+    private IMongoCollection<ProjetoModel> _projetosCollection;
+
+    public ProjetoRepository(IMongoCollection<ProjetoModel> projetosCollection) {
+        _projetosCollection = projetosCollection;
+    }
+
+    public async Task<IEnumerable<ProjetoModel>> GetAll() {
+        var filter = Builders<ProjetoModel>.Filter.Eq(DOC => DOC.dataInfoModel.active,true);
+        var cursor = await _projetosCollection.FindAsync(filter);
+        return await cursor.ToListAsync();
+    }
+
+    public async Task<ProjetoModel> GetById(string id) {
+
+        var filter = Builders<ProjetoModel>.Filter.Eq(DOC => DOC.dataInfoModel.active,true);
+        var filters = new List<FilterDefinition<ProjetoModel>>() {
+            Builders<ProjetoModel>.Filter.Eq(DOC => DOC.dataInfoModel.active,true),
+            Builders<ProjetoModel>.Filter.Eq(DOC => DOC._id, id)
+        };
+        var cursor = await _projetosCollection.FindAsync(Builders<ProjetoModel>.Filter.And(filters));
+        var result = cursor.FirstOrDefault<ProjetoModel>();
+        return result;
+    }
+}
