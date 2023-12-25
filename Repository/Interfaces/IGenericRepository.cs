a39e8badcc72534c09acbf284a27065177463051
Tales Xavier <talesxavier1@hotmail.com>
2023-12-25 00:21:40 -0300

    Melhoria. Criação de uma interface de Repositorio Genérico.

---
+++ Repository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Simples_Data.Repository.Interfaces;
+public interface IGenericRepository<T> {
+    public Task<IEnumerable<T>> GetAll();
+    public Task<T> GetById(string id);
+    public Task<bool> Existe(string id);
+    public Task<bool> tryAdd(T entity);
+    public Task<bool> tryUpdate(T entity);
+    public Task<bool> tryDelete(string id);
+}
--- Repository/Interfaces/IProjetoRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿using Simples_Data.Models;
-
-public interface IProjetoRepository {
-    public Task<IEnumerable<ProjetoModel>> GetAll();
-    public Task<ProjetoModel> GetById(string id);
-}
