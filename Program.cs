90af3f6214f7d43cbfc34147c9950ae1a97caf58
Tales Xavier <talesxavier1@hotmail.com>
2023-12-24 21:31:38 -0300

    Ajuste. Remoção de imports não usados. Remoção da inicialização do mongo no padrão singleton.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.OData;
 using MongoDB.Driver;
-using Simples_Data.APIs.TraceListeners;
 using Simples_Data.APIs_SimplesData_Front.Odata;
 using Simples_Data.MongoDB;
 using SingularChatAPIs.utils;
-using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
-/*MongoDBConnection.start();*/
-
-Trace.Listeners.Add(new LogTraceListener());
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen();
@@ -48,15 +43,5 @@ app.UseODataRouteDebug();
 app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 app.MapControllerRoute(name: "default",pattern: "{controller=main}/{action=Index}/{id?}");
-
-/*app.UseWhen(context => {
-    var value = context.Request.Path.Value;
-    if (value != null && value.Contains("api/v1/data")) { return true; }
-    return false;
-}
-,branch => {
-    branch.UsePipelineValidacaoRequisicao();
-});*/
-
 
 app.Run();
