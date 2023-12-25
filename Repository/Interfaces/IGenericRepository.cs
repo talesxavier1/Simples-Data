cb551a31e4e9de0b46d6d3567f6a2bfa69e78d83
Tales Xavier <talesxavier1@hotmail.com>
2023-12-25 02:49:02 -0300

    Ajuste. Ajuste na nome da função.

--- Repository/Interfaces/IGenericRepository.cs
+++ Repository/Interfaces/IGenericRepository.cs
@@ -2,7 +2,7 @@
 public interface IGenericRepository<T> {
     public Task<IEnumerable<T>> GetAll();
     public Task<T> GetById(string id);
-    public Task<bool> Existe(string id);
+    public Task<bool> Exist(string id);
     public Task<bool> tryAdd(T entity);
     public Task<bool> tryUpdate(T entity);
     public Task<bool> tryDelete(string id);
